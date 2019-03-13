91cebadecd10d56011f25d3fcc1aacfaaeebfb40
yuanliqiang <yuanliqiang@yuanliqiangdeMacBook-Pro.local>
2019-03-13 17:34:46 +0800

    flask

--- y2t.py
+++ y2t.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os, time, math
+import os, time, math, json
 from multiprocessing import Process
+from flask import Flask, request, jsonify
 
 import cnf
 import list
 import youtube
+
+app = Flask(__name__)
 
 
 def chunks(arr, m):
@@ -21,21 +24,27 @@ def foreach(proxy, displays):
         p.join()
 
 
+@app.route('/', methods=['POST'])
+def y2thandler():
+    if request.method == 'GET':
+        return jsonify({"success": "false"})
+    else:
+        displaylist =request.get_json()
+        arr = chunks(displaylist, len(cnf.PROXYLIST))
+        l = len(arr)
+        idx = 0
+        plist = []
+
+        #    for proxy in cnf.PROXYLIST:
+        #        youtube.checkproxy(proxy)
+
+        for proxy in cnf.PROXYLIST:
+            if idx < l:
+                p = Process(target=foreach, args=(proxy, arr[idx]))
+                idx = idx + 1
+                plist.append(p)
+                p.start()
+    return jsonify({"success": "true"})
+
 if __name__ == "__main__":
-    arr = chunks(list.DISPLAYLIST, len(cnf.PROXYLIST))
-    l = len(arr)
-    idx = 0
-    plist = []
-
-#    for proxy in cnf.PROXYLIST:
-#        youtube.checkproxy(proxy)
-
-    for proxy in cnf.PROXYLIST:
-        if idx < l:
-            p = Process(target=foreach, args=(proxy, arr[idx]))
-            idx = idx + 1
-            plist.append(p)
-            p.start()
-
-    for p in plist:
-        p.join()
+    app.run(host='127.0.0.1', port=8080)
