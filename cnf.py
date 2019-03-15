1d75ee0ba503cd9df30152e4723301d50ca64cb7
jiang <tallerc@sina.com>
2019-03-15 10:58:22 +0800

    first commit

--- cnf.py
+++ cnf.py
@@ -3,14 +3,14 @@
 
 BASEDIR = "/Users/yuanliqiang/workspace/y2t"
 HEADDIR = "/Users/yuanliqiang/workspace/y2t/movie"
-DATADIR = "/Volumes/SamsungTF/download"
+DATADIR = "/Users/yuanliqiang/www/dota2"
 YOUTUBEPARAM="-f '(bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4])' --buffer-size 16k --write-thumbnail --retries infinite"
 # YOUTUBEPARAM = "-f '(bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4])' --buffer-size 16k --retries infinite"
 YOUTUBESUB="--write-auto-sub --sub-lang=zh-Hans --convert-subs=srt"
 
 PROXYLIST = []
 
-PROXYLIST.append("127.0.0.1:1081")
+PROXYLIST.append("127.0.0.1:1080")
 #PROXYLIST.append("127.0.0.1:1081")
 #PROXYLIST.append("127.0.0.1:1082")
 #PROXYLIST.append("127.0.0.1:1083")
